999d08ad3946d8ce1743686d9dc2111c4ac5ddcd
Christian-Porras <chris.porras@storable.com>
2021-06-04 10:51:46 -0500

    add shoppingList

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,14 @@
+const ShoppingList = ({name}) => {
+  return (
+    <div className="shopping-list">
+      <h1>Shopping List for {name}</h1>
+      <ul>
+        <li>Instagram</li>
+        <li>WhatsApp</li>
+        <li>Oculus</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ShoppingList;
