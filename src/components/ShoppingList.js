fc32d93c989940005c76ab5b9a976979c6148707
João Fagundes <joaovictordmf@gmail.com>
2021-06-16 09:53:29 -0300

    add checkbox list and show completion

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const ShoppingList = ({name}) => {
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  list: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+const defaultProps = {
+
+}
+
+const ShoppingList = ({name, list}) => {
+  const [checked, setChecked] = useState(0);
+
+  const handleCheck = (event) => {
+    let aux = checked;
+
+    if (event.target.checked) {
+      aux += 1;
+    } else {
+      aux -= 1;
+    }
+
+    setChecked(aux);
+  }
+
+  const renderList = (shoppingList) => {
+    return shoppingList.map((item) => {
+      return (
+        <div key={item}>
+          <input type="checkbox" key={item} onChange={handleCheck} />
+          <label key={item}> {item} </label>
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="shopping-list">
       <h1>Shopping List for {name}</h1>
-      <ul>
-        <li>Instagram</li>
-        <li>WhatsApp</li>
-        <li>Oculus</li>
-      </ul>
+      <div>
+        {renderList(list)}
+      </div>
+
+      <span> Items in cart: {checked}/{list.length} </span>
     </div>
   )
 }
+
+ShoppingList.propTypes = propTypes;
+ShoppingList.defaultProps = defaultProps;
 
 export default ShoppingList;
--- src/containers/ShoppingListContainer.js
+++ src/containers/ShoppingListContainer.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ShoppingList from "../components/ShoppingList";
 
 const ShoppingListContainer = () => {
+  const item_list = [
+    'Milk',
+    'Eggs',
+    'Bacon',
+    'Soda'
+  ]
+
   return (
-    <ShoppingList name='Chris' />
+    <ShoppingList name='Chris' list={item_list} />
   )
 }
 
