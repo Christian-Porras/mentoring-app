b2fd60c0f0f26f752f5f65b56b0a2c27f6e8d7cc
Christian-Porras <chris.porras@storable.com>
2021-07-02 13:03:31 -0500

    set up skeleton for formik

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withFormik } from 'formik';
 
 const propTypes = {
   value: PropTypes.string.isRequired,
@@ -16,11 +17,12 @@ const ListForm = ({value, setValue}) => {
   }
 
   return (
-    <form>
+    <form /*onSubmit={handleSubmit} */>
       <label>
         Your name:
         <input type="text" name="shopping-list-form" value={value} onChange={handleChange}/>
       </label>
+      <button type="submit">Submit</button>
     </form>
   )
 }
@@ -28,4 +30,13 @@ const ListForm = ({value, setValue}) => {
 ListForm.propTypes = propTypes;
 ListForm.defaultProps = defaultProps;
 
-export default ListForm;
+export default withFormik({
+  // Map incoming values to our form values
+  // mapPropstoValues: (props) => (),
+
+  // Validation
+  // validate: values => {},
+
+  // Handle Submit
+  // handleSubmit: (values, { setSubmitting }) => {}
+})(ListForm);
